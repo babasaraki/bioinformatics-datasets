765d7e25130f5bf0121153c6962922d2934406cf
Alexander Pico <apico@gladstone.ucsf.edu>
2020-02-22 15:50:56 -0800

    new harmonizome content and functions

--- build-gene-sets.R
+++ build-gene-sets.R
@@ -4,33 +4,78 @@ library(dplyr)
 library(magrittr)
 
 ## List of dirs to process
-dir.list <- c("geo-hs-disease-gene-sets", "geo-mm-ko-gene-sets")
+buildGeoSet <- function(){
+  dir.list <- c("geo-hs-disease-gene-sets", "geo-mm-ko-gene-sets")
+  
+  lapply(dir.list, function(d){
+    file.list <- list.files(d, "\\.tsv")
+    
+    df.data <- data.frame(symbol = character(),
+                          log2fc = numeric(),
+                          pv = numeric(),
+                          fdr = numeric(),
+                          dataset_id = character(),
+                          dataset_annot = character())
+    
+    lapply(file.list, function(f){
+      fn.a <- strsplit(f,".tsv")[[1]]
+      fn.sp <- strsplit(fn.a, "__")[[1]]
+      id <- fn.sp[1]
+      annot <- fn.sp[2]
+      data <- read.table(file.path(d, f) , header = T, sep = "\t", stringsAsFactors = F)
+      
+      #Filter for significant p-values and non-blank symbols
+      sig.data <- data %>%
+        dplyr::filter(!Gene.symbol == "" & P.Value < 0.05) %>%
+        dplyr::select(Gene.symbol, logFC, P.Value, adj.P.Val) %>%
+        set_colnames(c("symbol", "log2fc", "pv", "fdr")) 
+      #Select median probe to handle duplicates
+      med.data <- sig.data %>%
+        dplyr::group_by(symbol) %>%
+        dplyr::summarise(log2fc = median(log2fc)) %>%
+        mutate(med.value = TRUE)
+      this.data <- merge(sig.data, med.data, by=c("symbol", "log2fc")) %>%
+        arrange(desc(log2fc)) %>%
+        mutate(sv = sign(log2fc)*-log(pv,10)) %>% ## Add standardValue
+        dplyr::select(-med.value)
+      
+      this.data$dataset_id = id
+      this.data$dataset_annot = annot
+      df.data <<- rbind(df.data, this.data)
+    })
+    
+    write.table(df.data, paste0(d, ".tsv"), sep = "\t", row.names = F)
+  })
+}
 
-lapply(dir.list, function(d){
-  file.list <- list.files(d, "\\.tsv")
+## Harmonizome
+buildHarmonizomeSets <- function() {
+  gams <- read.table("gene_attribute_matrix_standardized.txt", sep="\t", stringsAsFactors = F, header = T)
+  attr.list <- read.table("attribute_list_entries.txt", sep="\t", stringsAsFactors = F, header = T, quote="\"")
   
   df.data <- data.frame(symbol = character(),
-                        log2fc = numeric(),
-                        pv = numeric(),
-                        fdr = numeric(),
+                        sv = numeric(),
                         dataset_id = character(),
                         dataset_annot = character())
-  lapply(file.list, function(f){
-    fn <- f
-    fn.a <- strsplit(f,".tsv")[[1]]
-    fn.sp <- strsplit(fn.a, "__")[[1]]
-    id <- fn.sp[1]
-    annot <- fn.sp[2]
-    data <- read.table(file.path(d, fn) , header = T, sep = "\t", stringsAsFactors = F)
-    this.data <- data %>%
-      arrange(desc(logFC)) %>%
-      dplyr::filter(!Gene.symbol == "" & P.Value < 0.05) %>%
-      dplyr::select(Gene.symbol, logFC, P.Value, adj.P.Val) %>%
-      set_colnames(c("symbol", "log2fc", "pv", "fdr"))
+  
+  lapply(names(gams)[-c(1:3)], function(d){
+    annot.gse <- substring(d,2)
+    annot <- attr.list[which(attr.list$GSE == annot.gse),"Disease"]
+    id <- paste0("GSE",annot.gse)
+    
+    #Filter for significant p-values
+    this.data <- gams %>%
+      dplyr::filter(!!as.name(d) > -log(0.05,10) |
+                      !!as.name(d) < log(0.05,10)) %>%
+      dplyr::arrange(desc(!!as.name(d))) %>%
+      dplyr::select("GeneSym", d) %>%
+      set_colnames(c("symbol","sv"))
+
     this.data$dataset_id = id
     this.data$dataset_annot = annot
     df.data <<- rbind(df.data, this.data)
   })
   
-  write.table(df.data, paste0(d, ".tsv"), sep = "\t", row.names = F)
-})
+  write.table(df.data, "harmonizome-hs-disease-gene-sets.tsv", sep = "\t", row.names = F)
+
+}
--- query-gene-sets.R
+++ query-gene-sets.R
@@ -4,12 +4,13 @@ library(dplyr)
 library(magrittr)
 
 ## Read built gene sets
-disease.hs <- read.table("geo-hs-disease-gene-sets.tsv", sep = "\t", header = T, stringsAsFactors = F)
-ko.mm <- read.table("geo-mm-ko-gene-sets.tsv", sep = "\t", header = T, stringsAsFactors = F)
+geo.disease.hs <- read.table("geo-hs-disease-gene-sets.tsv", sep = "\t", header = T, stringsAsFactors = F)
+geo.ko.mm <- read.table("geo-mm-ko-gene-sets.tsv", sep = "\t", header = T, stringsAsFactors = F)
+harmonizome.disease.hs <- read.table("harmonizome-hs-disease-gene-sets.tsv", sep = "\t", header = T, stringsAsFactors = F)
 
 ## Functions
 
-### List Datasets
+### List GEO Datasets
 # Lists compiled datasets by disease and dataset ID with unique gene counts.
 #
 # sig.only    Only count genes with FDR < 0.05. Default is FALSE.
@@ -18,12 +19,12 @@ ko.mm <- read.table("geo-mm-ko-gene-sets.tsv", sep = "\t", header = T, stringsAs
 #
 # returns a tibble
 #
-# examples: listDatasets()
-#           listDatasets(T)
+# examples: listGeoSets()
+#           listGeoSets(T)
 #
-listDatasets <- function(sig.only = FALSE, pos.only = FALSE, by.dataset = TRUE){
+listGeoSets <- function(sig.only = FALSE, pos.only = FALSE, by.dataset = TRUE){
   
-  disease.hs %>%
+  geo.disease.hs %>%
     dplyr::filter(if (pos.only) log2fc > 0 else is.numeric(log2fc)) %>%
     dplyr::filter(if (sig.only) fdr < 0.05 else is.numeric(fdr)) %>%
     group_by(dataset_annot) %>%
@@ -32,27 +33,81 @@ listDatasets <- function(sig.only = FALSE, pos.only = FALSE, by.dataset = TRUE){
     }
 }
 
-### Genes By Dataset
+### Genes By GEO Dataset
 # Retrieve genes and associated data for a given dataset.
 #
 # dataset.id  ID of dataset for genes.
 # sig.only    Only count genes with FDR < 0.05. Default is FALSE.
 # pos.only    Only count genes with log2FC > 0. Default is FALSE.
 #
-# returns a datafame of symbol, log2fc, pv and fdr
+# returns a datafame of symbol, log2fc, pv, fdr and sv
 #
-# examples: genesByDataset("GSE6357")
-#           genesByDataset("GSE6357", T,T)
+# Details: sv are "standardizedValues" calculated as -log10(pv) * sign(log2fc).
+#   Theses values can be used for GSEA ranking or defining positive/negative significantly
+#   differentially expressed gene sets.
 #
-genesByDataset <- function(dataset.id = NULL, sig.only = FALSE, pos.only = FALSE){
+# examples: genesByGeoSet("GSE3467")
+#           genesByGeoSet("GSE3467", T,T)
+#
+genesByGeoSet <- function(dataset.id = NULL, sig.only = FALSE, pos.only = FALSE){
   
   if(is.null(dataset.id)){
-    stop("Must provide a dataset ID. See listDatasets().")
+    stop("Must provide a dataset ID. See listGeoSets().")
   }
   
-  disease.hs %>%
+  geo.disease.hs %>%
     dplyr::filter(dataset_id == dataset.id) %>%
     dplyr::filter(if (pos.only) log2fc > 0 else is.numeric(log2fc)) %>%
     dplyr::filter(if (sig.only) fdr < 0.05 else is.numeric(fdr)) %>%
-    dplyr::select(symbol, log2fc, pv, fdr)
+    dplyr::select(symbol, log2fc, pv, fdr, sv)
+}
+
+### List Harmonizome Datasets
+# Lists compiled datasets by disease and dataset ID with unique gene counts.
+# Details: "standardizedValues" provided by Harmonizome are -log10(p-value) * sign(log2FC).
+#   Theses values can be used for GSEA ranking or defining positive/negative significantly
+#   differentially expressed gene sets.
+#
+# pos.only    Only count genes with standardizedValue > 0. Default is FALSE.
+# by.dataset  Separate counts by dataset. Default is TRUE.
+#
+# returns a tibble
+#
+# examples: listHarmonizomeSets()
+#           listHarmonizomeSets(T)
+#
+listHarmonizomeSets <- function(pos.only = FALSE, by.dataset = TRUE){
+  
+  harmonizome.disease.hs %>%
+    dplyr::filter(if (pos.only) sv > 0 else is.numeric(sv)) %>%
+    group_by(dataset_annot) %>%
+    {if (by.dataset) count(., dataset_id, name = "unique_genes")
+      else summarise(., unqiue_genes = n_distinct(symbol))
+    }
+}
+
+### Genes By Harmonizome Dataset
+# Retrieve genes and associated data for a given dataset.
+# Details: "standardizedValues" provided by Harmonizome are -log10(p-value) * sign(log2FC).
+#   Theses values can be used for GSEA ranking or defining positive/negative significantly
+#   differentially expressed gene sets.
+#
+# dataset.id  ID of dataset for genes.
+# pos.only    Only count genes with standardizedValue > 0. Default is FALSE.
+#
+# returns a datafame of symbol and standardizedValue
+#
+# examples: genesByHarmonizomeSet("GSE3467")
+#           genesByHarmonizomeSet("GSE3467", T)
+#
+genesByHarmonizomeSet <- function(dataset.id = NULL, pos.only = FALSE){
+  
+  if(is.null(dataset.id)){
+    stop("Must provide a dataset ID. See listGeoSets().")
+  }
+  
+  harmonizome.disease.hs %>%
+    dplyr::filter(dataset_id == dataset.id) %>%
+    dplyr::filter(if (pos.only) sv > 0 else is.numeric(sv)) %>%
+    dplyr::select(symbol, sv)
 }
